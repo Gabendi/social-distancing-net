bd131848ac0be8fffd477ab9d63b3a2c0f6812c9
Anna Bodnar <anna.bodnar97@gmail.com>
2020-10-21 22:48:24 +0200

    rename n,m to with, height

--- video_frame.py
+++ video_frame.py
@@ -5,21 +5,21 @@ class VideoFrame:
 
     Attributes
     ---------- 
-        n : static int
+        width : static int
             width of a frame
-        m : static int
+        heigh t: static int
             height of a frame
-        pixels : n x m x 3 numpy array
+        pixels : width x height x 3 numpy array
             numpy array containing pixel values (dtype: int8)
     """
-    n=1280
-    m=720
+    width=1280
+    height=720
     def __init__(self,pixels):
         """
 
         Parameters
         ----------
-            pixels : n x m x 3 numpy array
+            pixels : width x height x 3 numpy array
                 numpy array containing pixel values (dtype: int8)
         """
         self.pixels=pixels
