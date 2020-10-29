daf02e4a713aef3caa580387a432f1ce25e34f93
PDomonkos <47611103+PDomonkos@users.noreply.github.com>
2020-10-29 15:05:22 +0100

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -29,8 +29,29 @@ class Tracker:
         
         raise NotImplementedError()
 
-    def groupTrajectories()->None:
+    def groupTrajectories(self, d = 2, dt = 4 * 30)->None:
         """
-        Updates groups
+        Considers two individuals as being in the same group if they are less then d meters apart for at least dt seconds.
+        Parameters
+        ----------
+            d : int
+                distance
+            dt : int
+                min seconds (sec * fps)
         """
-        raise NotImplementedError()
+        for i, p1 in enumerate(self.activePeople):
+            for j, p2 in enumerate(self.activePeople):
+                if (i > j) and (p1 not in p2.inGroupWith):
+                    t = 0
+                    max_t = 0
+                    for k in range(len(p1.coordinates)):
+                        if (p1.coordinates[k].DistanceFrom(p2.coordinates[k]) <= d):
+                            t += 1
+                            if t > max_t:
+                                max_t = t
+                        else:
+                            t = 0
+                    if max_t >= dt:
+                        p1.inGroupWith.append(p2)
+                        p2.inGroupWith.append(p1)
+
